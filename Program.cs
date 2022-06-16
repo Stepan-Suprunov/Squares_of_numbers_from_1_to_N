5e51975b24fe596bc90b63519ebc3dec50c34272
Stepan-Suprunov <ipsuprunovso@mail.ru>
2022-06-17 00:53:18 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Таблица квадратов чисел от 1 до N.
+
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (N >= i)
+{
+    int result = (int) Math.Pow(i, 2);
+    Console.Write(result + ", ");
+    i++;
+}
